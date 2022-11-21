a0fdf22b51113d8466003d5a10fbac0fc67680db
wilson@streamsets.com <wilson@streamsets.com>
2022-11-21 09:22:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
 
-streamlit.title('hello shamim')
-streamlit.header("menu")
-streamlit.text("simple text")
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
